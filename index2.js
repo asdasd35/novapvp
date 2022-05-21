ea63b09858be19f537ab76eda91e4c877fbf91d8
asdasd35 <88936560+asdasd35@users.noreply.github.com>
2022-05-21 20:57:16 +0200

    asd

--- index2.js
+++ index2.js
@@ -27,4 +27,31 @@ else
 {
     location.reload
 }
+    
+        if(username=="Hi_im_PRO"&& password=="Reka_egy_fasz")
+    {
+        setTimeout(() => {
+            window.location = 'webshop2.html'
+
+        }, 2);
+        return false
+    }
+else
+{
+    location.reload
+}
+    
+            if(username=="Zazi120318"&& password=="120318")
+    {
+        setTimeout(() => {
+            window.location = 'webshop2.html'
+
+        }, 2);
+        return false
+    }
+else
+{
+    location.reload
+}
+    
 }
